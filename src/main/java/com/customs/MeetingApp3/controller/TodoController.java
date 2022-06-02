7fa9e971981a0e92c700e20a1f717ee8ae24dbd9
fijarixar <fijar.alridho@customs.go.id>
2022-06-02 11:06:53 +0700

    tambahkan Get By Id

--- src/main/java/com/customs/MeetingApp3/controller/TodoController.java
+++ src/main/java/com/customs/MeetingApp3/controller/TodoController.java
@@ -2,11 +2,13 @@ package com.customs.MeetingApp3.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,7 @@ public class TodoController {
     @Autowired
     private TodoRepository todoRepo;
 
+    // read semua data
     @GetMapping("/todos")
     public ResponseEntity<?> getAllTodos() {
         List<TodoDTO> todos = todoRepo.findAll();
@@ -32,7 +35,7 @@ public class TodoController {
     }
 
 
-
+    // create data
     @PostMapping("/todos")
     public ResponseEntity<?> createTodo(@RequestBody TodoDTO todo) {
         try {
@@ -44,7 +47,16 @@ public class TodoController {
         }
     }
 
-
+    // read data by ID metode PathVariable  contoh : localhost/todos/15224
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+        if(todoOptional.isPresent()) {
+            return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Todo not found with id "+id, HttpStatus.NOT_FOUND);
+        }
+    }
 
 
 
